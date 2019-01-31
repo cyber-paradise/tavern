40c1c851e4794dbd8e602e438303b71acf3d2822
mingming.song <dustinsong@outlook.com>
2019-01-31 17:21:05 +0800

    Initial commit

---
+++ src/test/java/com/cyberparadise/world/tavern/AbstractTest.java
@@ -0,0 +1,13 @@
+package com.cyberparadise.world.tavern;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author mingming.song
+ */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public abstract class AbstractTest {
+}
